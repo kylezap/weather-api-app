2a6cfa2a6b9ddc5fa89a60341ab350128bc78be0
kylezap <kzapcic+github@gmail.com>
2024-04-16 01:33:10 -0400

    created the api calls for geocode and hooked up to display

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,47 +1,78 @@
 const formSubmit = $('#search');
 const input = $('#search-input')
 const right = $('#right')
+const cityContainer = $('city-info')
+const cityNameResult = $('#city-name-result')
+const cityTempResult = $('#city-temp-result')
+const cityWindResult = $('#city-wind-result')
+const cityHumidResult = $('#city-humid-result')
+
+let savedData = localStorage.getItem('search');
+// console.log(savedData)
 
 
+function convertTemp(num) {
+	const kelvin = num;
+	const celsius = kelvin - 273;
+	let fahrenheit = celsius * (9 / 5) + 32;
+	fahrenheit = Math.floor(fahrenheit);
+
+	return fahrenheit;
+}
 
 function apiCall(event) {
 	event.preventDefault();
-	
+	// savedData.push(input.val());
 	console.log("Inside API Call");
-	const url = `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=216b199ad40ea7f7a10905ebc665b34c`;
 	
+let inputValue = input.val();
 
-	fetch(url)
+//making first API call to receive geocode values
+
+	const cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&limit=1&appid=216b199ad40ea7f7a10905ebc665b34c`
+
+	fetch(cityUrl)
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (data) {
+		console.log(data);
+		let lat = data[0].lat;
+		let lon = data[0].lon;
+		console.log(lon);
+
+//API call using Lat and Lon found with first call
+
+		const weatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=216b199ad40ea7f7a10905ebc665b34c`;
+	fetch(weatherUrl)
 		.then(function (response) {
 			return response.json();
 		})
 		.then(function (data) {
-			localStorage.setItem('search', input.val());
-			const recipeContent = data.feed;
+			console.log(data.daily);
 
-			recipeContainer.empty();
+			localStorage.setItem('search', inputValue);
 
-			for (let i = 0; i < recipeContent.length; i++) {
+			right.css('display','block');
+			let citySearchInput = inputValue.toUpperCase();
+			cityNameResult.text(citySearchInput);
+			cityTempResult.text(`Today's High is ` + convertTemp(parseInt(data.daily[0].temp.max)));
+			cityWindResult.text('Wind: ' + (parseInt(data.daily[0].wind_speed)));
+			cityHumidResult.text('Humidity is: ' + (parseInt(data.daily[0].humidity)));
 
-				const recipeName = recipeContent[i].display.displayName;
-				const recipeImg = recipeContent[i].display.images[0];
-				const recipeUrl = recipeContent[i].display.source.sourceRecipeUrl
 
-				const recipeCard = $('<div>').addClass('card m-3').css('width', '18rem');
-				const cardImg = $('<img>').addClass('card-img-top').attr('src', recipeImg).css('height', '200px').css('object-fit', 'cover');
-				const cardBody = $('<div>').addClass('card-body d-flex flex-column');
-				const cardTitle = $('<h5>').addClass('card-title').text(recipeName);
-				const cardUrl = $('<a>').addClass('btn btn-primary mt-auto stretched-link').attr('href', recipeUrl).text('See Recipe');
 
-				cardImg.appendTo(recipeCard);
-				cardBody.appendTo(recipeCard);
-				cardTitle.appendTo(cardBody);
-				cardUrl.appendTo(cardBody);
-				recipeCard.appendTo(recipeContainer);
+			for (let i = 0; i < 5; i++) {
+				const maxTemp = parseInt(data.daily[i].temp.max);
+				const windSpeed = parseInt(data.daily[i].wind_speed);
+				const humidityRating = parseInt(data.daily[i].humidity);
+
+				console.log(`Humidity is ${humidityRating}%`);
+				console.log(`Windspeed is ${windSpeed} mph`);
+				console.log(`Today's high is ${convertTemp(maxTemp)}°`);
 			}
 		});
-
+	})
 };
-
 
 formSubmit.on('submit', apiCall);
